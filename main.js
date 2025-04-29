0d6d2a1b49a4086392a197a6ff61d8704ffd094f
Gabriel <gayv4468@gmail.com>
2025-04-29 13:05:51 +0000

    3

--- main.js
+++ main.js
@@ -20,3 +20,26 @@ let numero2 = 23
 somar(numero1, numero2)
 somar(30, 12)
 somar(19, 289)
+
+
+
+function raizQuadrada(numero) {
+  console.log(Math.sqrt(numero))
+}
+
+function potenciacao(valor1, valor2) {
+  console.log(Math.pow(valor1, valor2))
+}
+
+function multiplicacao(valor1, valor2) {
+    console.log(valor1 * valor2)
+}
+
+
+let numero1 = Number(prompt("Digite o numero 1:"))
+let numero2 = Number(prompt("Digite o numero 2:"))
+
+raizQuadrada(numero1)
+potenciacao(numero1, numero2)
+multiplicacao(numero2, numero1)
+
