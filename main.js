f410ca253da0f5fe73e2c9f9df59c684baa08169
Gabriel <gayv4468@gmail.com>
2025-04-29 12:03:09 +0000

    2

--- main.js
+++ main.js
@@ -5,3 +5,18 @@ function mensagem(){
 }
 
 mensagem()
+
+
+
+
+function somar(numero1, numero2) {
+  console.log(numero1 + numero2)
+
+}
+
+let numero1 = 40
+let numero2 = 23
+
+somar(numero1, numero2)
+somar(30, 12)
+somar(19, 289)
